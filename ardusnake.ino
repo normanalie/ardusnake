1767acf924aaed7e2c7787c6478526275e2fe101
Norman Alié <mail@normanalie.fr>
2022-12-23 13:54:29 +0100

    Wall collisions with animations

--- ardusnake.ino
+++ ardusnake.ino
@@ -3,13 +3,14 @@
 
 //Display snake OK
 //Move snake forward OK
-//Collide wall
-//Collide self
+//Collide wall OK
 //Direction
+//Grow
+//Collide self
 //Generate apple
 //Eat apple
 
-#define TICK 10
+#define TICK 1
 #define FPS 30
 
 #define MATRIX_DIN 3
@@ -27,8 +28,10 @@ unsigned long prev_frame = millis();
 unsigned long prev_tick = millis();
 
 void test_pattern();
+int init_game(); // Called at the end of setup
 int update_game();  // Called every tick
 int update_screen(); // Called every frame
+int end_game(); // Called at the end of the game
 
 void setup() {
   pinMode(UP, INPUT_PULLUP);
@@ -40,11 +43,7 @@ void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 2);
   //test_pattern();
-
-  snake.x = 4;
-  snake.y = 3;
-  snake.dir = RIGHT;
-  snake.next = NULL;
+  init_game();
 }
 
 void loop() {
@@ -64,16 +63,32 @@ void test_pattern(){
   for(int j = 0; j < 8; j++){
     for(int i = 0; i < 8; i++){
       lc.setLed(0, i, j, HIGH);
-      delay(20);
+      delay(15);
     }
   }
   for(int j = 0; j < 8; j++){
     for(int i = 0; i < 8; i++){
       lc.setLed(0, i, j, LOW);
-      delay(20);
+      delay(15);
     }
   }
   return;
+}
+
+
+int init_game(){
+  test_pattern();
+  delay(1000);
+
+  snake.x = 4;
+  snake.y = 3;
+  snake.dir = RIGHT;
+  snake.next = NULL;
+
+  prev_frame = millis();
+  prev_tick = millis();
+
+  return 0;
 }
 
 
@@ -94,15 +109,19 @@ int update_game(){
   }
 
   if(snake.x > 7){
-    snake.x = 0;
-  }else if(snake.x < 0){
     snake.x = 7;
+    end_game();
+  }else if(snake.x < 0){
+    snake.x = 0;
+    end_game();    
   }
   
   if(snake.y > 7){
-    snake.y = 0;
-  }else if(snake.y < 0){
     snake.y = 7;
+    end_game();
+  }else if(snake.y < 0){
+    snake.y = 0;
+    end_game();
   }
   return 0;
 }
@@ -110,5 +129,12 @@ int update_game(){
 int update_screen(){
   lc.clearDisplay(0);
   lc.setLed(0, snake.x, snake.y, HIGH);
+  return 0;
+}
+
+
+int end_game(){
+  delay(200);
+  init_game();
   return 0;
 }
